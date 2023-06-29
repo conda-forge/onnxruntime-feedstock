9093ac47192884b20fac7639828a16a552d089fb
Silvio Traversaro <silvio@traversaro.it>
2023-06-29 14:22:39 +0200

    Update install-cpp.sh

--- recipe/install-cpp.sh
+++ recipe/install-cpp.sh
@@ -10,8 +10,8 @@ if [[ -n "${OSX_ARCH:+yes}" ]]; then
     install build-ci/Release/libonnxruntime.*dylib "${PREFIX}/lib"
 else
     install build-ci/Release/libonnxruntime.so* "${PREFIX}/lib"
-    install build-ci/Release/libonnxruntime_providers_shared.so* "${PREFIX}/lib"
     if [[ ! -z "${cuda_compiler_version+x}" && "${cuda_compiler_version}" != "None" ]]; then
+        install build-ci/Release/libonnxruntime_providers_shared.so* "${PREFIX}/lib"
         install build-ci/Release/libonnxruntime_providers_cuda.so* "${PREFIX}/lib"
     fi
 fi
