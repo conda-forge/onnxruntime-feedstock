da7f0413fe8019a6360ee721920e87a7622437f0
Silvio Traversaro <silvio@traversaro.it>
2023-06-29 14:09:57 +0200

    Update install-cpp.bat

--- recipe/install-cpp.bat
+++ recipe/install-cpp.bat
@@ -7,3 +7,10 @@ mkdir "%PREFIX%\Library\bin"
 xcopy /E /I include\onnxruntime "%PREFIX%\Library\include\onnxruntime"
 xcopy /Y build-ci\Release\onnxruntime_conda.lib "%PREFIX%\Library\lib\"
 xcopy /Y build-ci\Release\onnxruntime_conda.dll "%PREFIX%\Library\bin\"
+xcopy /Y build-ci\Release\onnxruntime_providers_shared.lib "%PREFIX%\Library\lib\"
+xcopy /Y build-ci\Release\onnxruntime_providers_shared.dll "%PREFIX%\Library\bin\"
+
+if NOT "%cuda_compiler_version%"=="None" (
+    xcopy /Y build-ci\Release\onnxruntime_providers_conda.lib "%PREFIX%\Library\lib\"
+    xcopy /Y build-ci\Release\onnxruntime_providers_conda.dll "%PREFIX%\Library\bin\"
+)
