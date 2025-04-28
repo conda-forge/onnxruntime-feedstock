900af8aacc83e73eeba5795fcc65439318688bdf
Mark Harfouche <mark.harfouche@gmail.com>
2025-04-28 11:02:13 -0400

    define minimum cmake policy

--- recipe/build.sh
+++ recipe/build.sh
@@ -53,14 +53,15 @@ else
 fi
 
 cmake_extra_defines=( "EIGEN_MPL2_ONLY=ON" \
-		      "FLATBUFFERS_BUILD_FLATC=OFF" \
-	              "onnxruntime_USE_COREML=OFF" \
+                      "FLATBUFFERS_BUILD_FLATC=OFF" \
+                      "onnxruntime_USE_COREML=OFF" \
                       "onnxruntime_DONT_VECTORIZE=$DONT_VECTORIZE" \
                       "onnxruntime_BUILD_SHARED_LIB=ON" \
                       "onnxruntime_BUILD_UNIT_TESTS=$BUILD_UNIT_TESTS" \
                       "CMAKE_PREFIX_PATH=$PREFIX" \
-		      "CMAKE_CUDA_ARCHITECTURES=all-major"
-		    )
+                      "CMAKE_CUDA_ARCHITECTURES=all-major" \
+                      "CMAKE_POLICY_VERSION_MINIMUM=3.5"
+)
 
 # Copy the defines from the "activate" script (e.g. activate-gcc_linux-aarch64.sh)
 # into --cmake_extra_defines.
